a7c309af71282f01f267799f595cc0c9b66de523
Udit Jain <udjai@microsoft.com>
2020-06-16 23:44:07 +0530

    Changed the SQL script

--- EmailSchedulerApp/dbo.tb_emails.sql
+++ EmailSchedulerApp/dbo.tb_emails.sql
@@ -1,19 +1,9 @@
-USE [DBEmailScheduler]
-GO
-
-/****** Object: Table [dbo].[tb_emails] Script Date: 16-06-2020 23:27:16 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[tb_emails] (
     [Id]                    INT            IDENTITY (1, 1) NOT NULL,
     [emailID]               NVARCHAR (MAX) NOT NULL,
-    [isOpened]              INT            NULL,
-    [isFirstEmailSent]      INT            NULL,
-    [remainingReminderDays] INT            NULL
+    [isOpened]              INT            DEFAULT ((0)) NULL,
+    [isFirstEmailSent]      INT            DEFAULT ((0)) NULL,
+    [remainingReminderDays] INT            DEFAULT ((-1)) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-
 
